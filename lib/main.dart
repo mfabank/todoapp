176fe67545a58e85ff0683844035074baa4c173b
MFA <53713274+mfabank@users.noreply.github.com>
2020-09-25 02:28:20 +0300

    Dismissible work!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:todoapp/models/gorevler.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -16,12 +15,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String yazi;
   String sure;
-  List <Gorevler> gorevler = [Gorevler("", "")];
   List gorevListem = [];
   List sureListem = [];
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +31,7 @@ class _MyAppState extends State<MyApp> {
       body: Column(
         children: [
           Container(
-            margin: EdgeInsets.only(top:20),
+            margin: EdgeInsets.only(top: 20),
             child: Row(
               children: [
                 Flexible(
@@ -46,8 +41,14 @@ class _MyAppState extends State<MyApp> {
                         width: 1000,
                         child: TextFormField(
                           decoration: InputDecoration(
+                            labelText: "Başlık",
+                            labelStyle:
+                                TextStyle(color: Colors.deepPurpleAccent),
                             isDense: true,
-                            icon: Icon(Icons.remove,color: Colors.deepPurpleAccent,),
+                            icon: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.deepPurpleAccent,
+                            ),
                             hintText: "Görev başlığı",
                           ),
                           style: TextStyle(
@@ -64,15 +65,21 @@ class _MyAppState extends State<MyApp> {
                         width: 1000,
                         child: TextFormField(
                           decoration: InputDecoration(
+                            labelText: "Detay",
+                            labelStyle:
+                                TextStyle(color: Colors.deepPurpleAccent),
                             isDense: true,
-                            icon: Icon(Icons.remove,color: Colors.deepPurpleAccent,),
+                            icon: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.deepPurpleAccent,
+                            ),
                             hintText: "Görev detayı",
                           ),
                           style: TextStyle(
                             color: Colors.deepPurpleAccent,
                           ),
                           onChanged: (String value) {
-                            sure=value;
+                            sure = value;
                           },
                           maxLength: 50,
                           cursorColor: Colors.deepPurpleAccent,
@@ -80,21 +87,22 @@ class _MyAppState extends State<MyApp> {
                       ),
                       SizedBox(height: 10),
                     ],
-
                   ),
                 ),
-
                 Container(
-                  padding: EdgeInsets.only(top:25),
+                  padding: EdgeInsets.only(top: 25),
                   child: FlatButton(
-                    child: Icon(Icons.post_add,size: 50,color: Colors.deepPurpleAccent,),
-                    onPressed: () {
-                      setState(() {
-                        gorevListem.add(yazi);
-                        sureListem.add(sure);
-                      });
-                    }
-                  ),
+                      child: Icon(
+                        Icons.post_add,
+                        size: 50,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          gorevListem.add(yazi);
+                          sureListem.add(sure);
+                        });
+                      }),
                 ),
               ],
             ),
@@ -103,12 +111,34 @@ class _MyAppState extends State<MyApp> {
             child: ListView.builder(
               itemCount: gorevListem.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Icon(Icons.calendar_today),
+                final silinecekMetin = gorevListem[index];
+                return Dismissible(
+                  direction: DismissDirection.horizontal,
+                  resizeDuration: Duration(seconds: 1),
+                  key: UniqueKey(),
+                  background: Container(
+                    color: Colors.deepPurpleAccent,
+                    child: Icon(Icons.edit),
+                  ),
+                  secondaryBackground: Container(
+                    color: Colors.red,
+                    child: Icon(Icons.delete),
+                  ),
+                  onDismissed: (direction){
+                    if(direction == DismissDirection.endToStart){
+                      gorevListem.removeAt(index);
+                      Scaffold.of(context).showSnackBar(SnackBar(content: Text("$silinecekMetin silindi")));
+                    }else if(direction == DismissDirection.startToEnd) {
+
+                    }
+                  },
+                  child: ListTile(
+                    leading: Icon(Icons.calendar_today),
                     title: Text(gorevListem[index]),
                     subtitle: Text(sureListem[index]),
-                  dense: true,
-                  );
+                    dense: true,
+                  ),
+                );
               },
             ),
           )
